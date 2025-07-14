0895a676ead59149e6c9c268091f2e4f438da30d
V1V1V112 <46706221+V1V112@users.noreply.github.com>
2025-07-14 19:14:41 +0800

    添加网络测速 snmp sftp 插件

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -47,15 +47,15 @@ PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 #24.10
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
-#PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
-PACKAGES="$PACKAGES luci-app-openclash"
-PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+# 代理工具 passwall openclash homeproxy
+#PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+PACKAGES="$PACKAGES luci-app-openclash"
+#PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 #### 原仓库基础添加 ####
 # snmpd插件针对于ikuai+op mac地址重复
 PACKAGES="$PACKAGES snmpd"
-PACKAGES="$PACKAGES openssh-sftp-server"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
